c1644cf2922f8c2205fd28afeaec028371e3d0d4
bh6539 <bh6539@naver.com>
2023-04-21 17:05:32 +0900

    test

--- web/config/settings.py
+++ web/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w_*&2b2x7)=w_lrc%hw49r*w(9lh4tup1k#7qr_sg=0!$o5cge
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0','localhost','127.0.0.1','pythonweb-vw7tpemekq-de.a.run.app']
 
 
 # Application definition
