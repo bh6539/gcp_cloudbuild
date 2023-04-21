3f4e1a157bc40f31f75b3094a3d8f20e10d377c0
bh6539 <bh6539@naver.com>
2023-04-21 17:02:01 +0900

    test

--- web/config/settings.py
+++ web/config/settings.py
@@ -76,15 +76,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'board',
-    'USER': 'postgres',
-    'PASSWORD': 'test',
-    'HOST': '34.64.201.252',
-    'PORT': '5432',
-  }
-
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
